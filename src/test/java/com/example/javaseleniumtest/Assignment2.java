4997b85a20eb28b4c6d17a41cba67bb26bd499a7
Diego Grisales <54412831+dfgrisales5078@users.noreply.github.com>
2023-03-11 16:52:17 -0500

    updated Assignment2.java

--- src/test/java/com/example/javaseleniumtest/Assignment2.java
+++ src/test/java/com/example/javaseleniumtest/Assignment2.java
@@ -22,39 +22,55 @@ public class Assignment2 {
         //--------------   Add/Delete Web Element  --------------
         safariDriver.get("https://the-internet.herokuapp.com/add_remove_elements/");
         Thread.sleep(4000);
-        WebElement addElementButton = safariDriver.findElement(By.xpath("//*[@id=\"content\"]/div/button"));
+        WebElement addElementButton = safariDriver.findElement(
+                By.xpath("//*[@id=\"content\"]/div/button"));
         addElementButton.click();
         Thread.sleep(4000);
-        WebElement deleteElementButton = safariDriver.findElement(By.xpath("//*[@id=\"elements\"]/button"));
+        WebElement deleteElementButton = safariDriver.findElement(
+                By.xpath("//*[@id=\"elements\"]/button"));
         deleteElementButton.click();
 
         //--------------   Enter text in text and search button  --------------
         safariDriver.get("https://demo.automationtesting.in/Register.html");
         // Enter first and last name using xpath
-        safariDriver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[1]/input")).sendKeys("John");
-        safariDriver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[2]/input")).sendKeys("Doe");
+        safariDriver.findElement(
+                By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[1]/input"))
+                .sendKeys("John");
+        safariDriver.findElement(
+                By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[2]/input"))
+                .sendKeys("Doe");
         Thread.sleep(4000);
         // clear first and last name text
-        safariDriver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[1]/input")).clear();
-        safariDriver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[2]/input")).clear();
+        safariDriver.findElement(
+                By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[1]/input")).clear();
+        safariDriver.findElement(
+                By.xpath("//*[@id=\"basicBootstrapForm\"]/div[1]/div[2]/input")).clear();
         Thread.sleep(4000);
 
         // Enter email and phone number using cssSelector
         safariDriver.findElement(By.cssSelector("#eid > input")).sendKeys("email@gmail.com");
 
         // Enter phone number using xpath
-        safariDriver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[4]/div/input")).sendKeys("2397777777");
+        safariDriver.findElement(
+                By.xpath("//*[@id=\"basicBootstrapForm\"]/div[4]/div/input"))
+                .sendKeys("2397777777");
 
         //--------------   Checkboxes and Radio buttons   --------------
-        safariDriver.findElement(By.cssSelector("#basicBootstrapForm > div:nth-child(5) > div > label:nth-child(1) > input")).click();
+        safariDriver.findElement(
+                By.cssSelector("#basicBootstrapForm > div:nth-child(5) > div > label:nth-child(1) > input")).click();
 
         for (int i = 1; i <= 3; i++) {
             safariDriver.findElement(By.id("checkbox" + i)).click();
         }
 
+        safariDriver.findElement(By.id("Button1")).click();
+        Thread.sleep(4000);
+
         //--------------   Dropdown Menus  --------------
         safariDriver.get("https://leafground.com/select.xhtml;jsessionid=node017poao36bf3jpf8z96wy3hxk1135438.node0");
-        Select fav_ui_automation_tool_dropdown = new Select(safariDriver.findElement(By.className("ui-selectonemenu")));
+        Thread.sleep(4000);
+        Select fav_ui_automation_tool_dropdown = new Select(safariDriver.findElement(
+                By.className("ui-selectonemenu")));
         fav_ui_automation_tool_dropdown.selectByVisibleText("Selenium");
         Thread.sleep(4000);
 
@@ -65,6 +81,12 @@ public class Assignment2 {
         WebElement drag = safariDriver.findElement(By.id("draggable"));
         WebElement drop = safariDriver.findElement(By.id("droppable"));
         action.dragAndDrop(drag, drop).build().perform();
+        Thread.sleep(4000);
+
+        // --------------  Click on using Link Text & Partial Link Text  --------------
+        safariDriver.findElement(By.linkText("Accept")).click();
+        Thread.sleep(4000);
+        safariDriver.findElement(By.partialLinkText("Propogation")).click();
         Thread.sleep(4000);
 
         // -------------- Get the title and current url of the selected webpage --------------
